c1b71c02a3f8085fa504eda092796a3fd02cf061
yozu <yocool0911@gmail.com>
2022-08-18 17:21:36 +0800

    demo01

---
+++ 01_左右圖片切換/src/main.js
@@ -0,0 +1,46 @@
+let next = document.querySelector('.next');
+let pre = document.querySelector('.pre');
+let img = document.querySelectorAll('.silde img');
+let now = 0;
+
+img.forEach((item)=>{
+     item.addEventListener('click',()=>{
+        alert('hi');
+     })
+})
+
+setInterval(next_btn, 3000);
+ next.addEventListener('click',next_btn,false)
+
+
+function next_btn(){
+    now++;
+    if(now >= img.length){
+      now = 0;
+    }
+    updata();
+}
+
+pre.addEventListener('click',()=>{
+
+    now--;
+    if(now  < 0 ){
+      now =  img.length-1;
+    }
+    updata();
+})
+
+
+function updata(){
+    img.forEach((item,key,)=>{
+        if(key == now){
+          item.classList.add('active');
+        }else{
+            item.classList.remove('active');
+        }
+      })
+}
+
+
+
+updata();
